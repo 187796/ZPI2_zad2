e13071acaa11fe7d3bffb658d71ba3f4443221eb
Krystian Ryniec <187796@edu.p.lodz.pl>
2016-10-24 13:26:44 +0200

    Dodanie metody readData

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,7 +6,11 @@ import java.util.Scanner;
  * Created by 187796 on 10/18/2016.
  */
 public class App {
+
     private static Scanner input = new Scanner(System.in);
+    private static String wrongString;
+    private static String goodString;
+    private static String fileName;
 
     public static void main(String[] args) {
 
@@ -21,5 +25,11 @@ public class App {
     private static String readValue(String message) {
         System.out.println(message);
         return input.nextLine();
+    }
+
+    public static void readData() {
+        fileName = readValue("podaj nazwe pliku");
+        goodString = readValue("podaj poprawny string");
+        wrongString = readValue("podaj niepoprawny string");
     }
 }
