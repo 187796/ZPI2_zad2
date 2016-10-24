7ce0fb82bd9a17b70c91f773e0ad556dde38573a
Damian Bartos <damian483@wp.pl>
2016-10-24 14:43:05 +0200

    fix implementacji callback

--- src/main/java/impl/CallBackImpl.java
+++ src/main/java/impl/CallBackImpl.java
@@ -3,15 +3,14 @@ package impl;
 import api.Callback;
 
 import java.util.Scanner;
-import java.util.concurrent.Callable;
 
 /**
  * Created by Damian Bartos
  * 24.10.2016.
  */
-public class CallBackImpl implements Callback, Callable<Boolean> {
+public class CallBackImpl implements Callback{
     @Override
-    public Boolean call() throws Exception {
+    public Boolean call()  {
         Scanner scanner = new Scanner(System.in);
         System.out.println("czy ponowic zapis t/n");
         return scanner.next().toLowerCase().equals("t");
