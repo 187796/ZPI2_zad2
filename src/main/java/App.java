bb3c3c52b83d025cc6a17d14a1038d9147fc6f5b
Krystian Ryniec <187796@edu.p.lodz.pl>
2016-10-24 13:21:42 +0200

    Dodanie metody readValue

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,16 +1,25 @@
 import api.Callback;
 
+import java.util.Scanner;
+
 /**
  * Created by 187796 on 10/18/2016.
  */
 public class App {
+    private static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        Callback callback;
+
         Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
             public void run() {
                 //callback.call();
             }
         }));
+    }
+
+    private static String readValue(String message) {
+        System.out.println(message);
+        return input.nextLine();
     }
 }
