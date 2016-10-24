03402b224fa4650a60d75d08ea57cae88dd85b90
Damian Bartos <damian483@wp.pl>
2016-10-24 14:28:00 +0200

    dodanie implementacji callback

--- src/main/java/impl/CallBackImpl.java
+++ src/main/java/impl/CallBackImpl.java
@@ -2,9 +2,18 @@ package impl;
 
 import api.Callback;
 
+import java.util.Scanner;
+import java.util.concurrent.Callable;
+
 /**
  * Created by Damian Bartos
  * 24.10.2016.
  */
-public class CallBackImpl implements Callback {
+public class CallBackImpl implements Callback, Callable<Boolean> {
+    @Override
+    public Boolean call() throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("czy ponowić zapis t/n");
+        return scanner.next().toLowerCase().equals("t");
+    }
 }
